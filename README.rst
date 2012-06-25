b82fcba8a42b0d63be86ac2c1668cab299ae5e1d
Reinout van Rees <reinout@vanrees.org>
2012-06-25 11:13:42 +0200

    Started a bit of documentation

--- README.rst
+++ README.rst
@@ -1,7 +1,16 @@
 lizard-geodin
 ==========================================
 
-Introduction
+`GeODin <http://www.geodin.com/>`_ (made by `Fugro <http://www.fugro.com/>`_)
+is a modular data management solution. Lizard-geodin is a Lizard application
+that couples Lizard and Geodin via Geodin's REST API.
 
-Usage, etc.
+The interaction with the REST API is on three separate levels:
 
+- Projects, location types etcetera rarely change. This static data can
+  basically be synced to the Lizard database.
+
+- The actual measures should be renewed often or even on the fly: they contain
+  the x/y location and the last value.
+
+- For every location, Geodin can give the recent historical data values.
--- setup.py
+++ setup.py
@@ -15,28 +15,23 @@ install_requires = [
     'lizard-ui >= 4.0b5',
     ],
 
-tests_require = [
-    ]
-
 setup(name='lizard-geodin',
       version=version,
-      description="TODO",
+      description="Coupling between Geodin's REST API and Lizard",
       long_description=long_description,
       # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=['Programming Language :: Python',
                    'Framework :: Django',
                    ],
-      keywords=[],
-      author='TODO',
-      author_email='TODO@nelen-schuurmans.nl',
-      url='',
+      keywords=['lizard', 'geodin'],
+      author='Reinout van Rees',
+      author_email='reinout.vanrees@nelen-schuurmans.nl',
+      url='https://github.com/lizardsystem/lizard-geodin',
       license='GPL',
       packages=['lizard_geodin'],
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
-      tests_require=tests_require,
-      extras_require = {'test': tests_require},
       entry_points={
           'console_scripts': [
           ]},
