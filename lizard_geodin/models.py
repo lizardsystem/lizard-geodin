cfe5d1b80fa408fa7386347c7ef21f656f4bd6fe
Reinout van Rees <reinout@vanrees.org>
2012-06-29 15:00:14 +0200

    Swapped the order of the models in the file so that I can point at subtypes later

--- lizard_geodin/models.py
+++ lizard_geodin/models.py
@@ -44,6 +44,100 @@ class Common(models.Model):
         self.save()
 
 
+class DataType(Common):
+    """Type of measure that has been done.
+
+    You need to do something with an investigation type. The data type tells
+    you what you did with it, like analyzing it in a geotechnical lab. It
+    results in a set of parameters like "dx=..., dy=..., dz=...".
+    """
+    investigation_type = models.ForeignKey(
+        'InvestigationType',
+        null=True,
+        blank=True,
+        related_name='data_types')
+
+    # Probably TODO: add parameters via extra json field? Including their
+    # description?
+
+    class Meta:
+        verbose_name = _('data type')
+        verbose_name_plural = _('data types')
+
+
+class InvestigationType(Common):
+    """Source of the measures.
+
+    Source means where the measure physically came from. A ground sample, for
+    instance.
+    """
+    location_type = models.ForeignKey(
+        'LocationType',
+        null=True,
+        blank=True,
+        related_name='investigation_types')
+
+    class Meta:
+        verbose_name = _('investigation type')
+        verbose_name_plural = _('investigation types')
+
+
+class LocationType(Common):
+    """Unknown; seems to be for setting attributes."""
+    project = models.ForeignKey(
+        'Project',
+        null=True,
+        blank=True,
+        related_name='location_types')
+
+    class Meta:
+        verbose_name = _('location type')
+        verbose_name_plural = _('location types')
+
+
+class Project(Common):
+    """Geodin project, it is the starting point for the API."""
+    id_field = 'prj_id'
+    field_mapping = (('source_url', 'prj_url'),
+                     ('name', 'prj_name'))
+
+    # TODO: field for location of project? For the ProjectsOverview page?
+    active = models.BooleanField(
+        _('active'),
+        help_text=_("Is the project used in this site?"),
+        default=False)
+    source_url = models.URLField(
+        _('source url'),
+        help_text=_(
+            "Geodin URL for automatically loading this project's data."),
+        null=True,
+        blank=True)
+    api_starting_point = models.ForeignKey(
+        'ApiStartingPoint',
+        null=True,
+        blank=True,
+        related_name='location_types')
+
+    class Meta:
+        verbose_name = _('project')
+        verbose_name_plural = _('projects')
+        ordering = ('-active', 'name')
+
+    def get_absolute_url(self):
+        return reverse('lizard_geodin_project_view',
+                       kwargs={'slug': self.slug})
+
+    def load_from_geodin(self):
+        """Load our data from the Geodin API."""
+        if not self.source_url:
+            raise ValueError("We need a source_url to update ourselves from.")
+        response = requests.get(self.source_url)
+        if response.json is None:
+            msg = "No json found. HTTP status code was %s, text was \n%s"
+            raise ValueError(msg % (response.status_code, response.text))
+        self.update_from_json(response.json)
+
+
 class ApiStartingPoint(Common):
     """Pointer at the Geodin API startpoint.
 
@@ -77,105 +171,10 @@ class ApiStartingPoint(Common):
         for json_project in response.json:
             project, created = Project.objects.get_or_create(
                 api_starting_point=self,
-                slug=json_project['prj_id'])
+                slug=json_project[Project.id_field])
             project.update_from_json(json_project)
             loaded_projects_ids.append(project.id)
         for unknown_project in Project.objects.exclude(
             pk__in=loaded_projects_ids):
-            unknown_project.active=False
+            unknown_project.active = False
             unknown_project.save()
-
-
-class Project(Common):
-    """Geodin project, it is the starting point for the API."""
-    id_field = 'prj_id'
-    field_mapping = (('source_url', 'prj_url'),
-                     ('name', 'prj_name'))
-
-    # TODO: field for location of project? For the ProjectsOverview page?
-    active = models.BooleanField(
-        _('active'),
-        help_text=_("Is the project used in this site?"),
-        default=False)
-    source_url = models.URLField(
-        _('source url'),
-        help_text=_(
-            "Geodin URL for automatically loading this project's data."),
-        null=True,
-        blank=True)
-    api_starting_point = models.ForeignKey(
-        ApiStartingPoint,
-        null=True,
-        blank=True,
-        related_name='location_types')
-
-    class Meta:
-        verbose_name = _('project')
-        verbose_name_plural = _('projects')
-        ordering = ('-active', 'name')
-
-    def get_absolute_url(self):
-        return reverse('lizard_geodin_project_view',
-                       kwargs={'slug': self.slug})
-
-    def load_from_geodin(self):
-        """Load our data from the Geodin API."""
-        if not self.source_url:
-            raise ValueError("We need a source_url to update ourselves from.")
-        response = requests.get(self.source_url)
-        if response.json is None:
-            msg = "No json found. HTTP status code was %s, text was \n%s"
-            raise ValueError(msg % (response.status_code, response.text))
-        self.update_from_json(response.json)
-
-
-class LocationType(Common):
-    """Unknown; seems to be for setting attributes."""
-    project = models.ForeignKey(
-        Project,
-        null=True,
-        blank=True,
-        related_name='location_types')
-
-    class Meta:
-        verbose_name = _('location type')
-        verbose_name_plural = _('location types')
-
-
-class InvestigationType(Common):
-    """Source of the measures.
-
-    Source means where the measure physically came from. A ground sample, for
-    instance.
-    """
-    location_type = models.ForeignKey(
-        LocationType,
-        null=True,
-        blank=True,
-        related_name='investigation_types')
-
-    class Meta:
-        verbose_name = _('investigation type')
-        verbose_name_plural = _('investigation types')
-
-
-class DataType(Common):
-    """Type of measure that has been done.
-
-    You need to do something with an investigation type. The data type tells
-    you what you did with it, like analyzing it in a geotechnical lab. It
-    results in a set of parameters like "dx=..., dy=..., dz=...".
-    """
-    investigation_type = models.ForeignKey(
-        InvestigationType,
-        null=True,
-        blank=True,
-        related_name='data_types')
-
-    # Probably TODO: add parameters via extra json field? Including their
-    # description?
-
-    class Meta:
-        verbose_name = _('data type')
-        verbose_name_plural = _('data types')
-
